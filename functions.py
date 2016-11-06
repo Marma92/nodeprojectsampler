fd3078ec2c1e870b146e19acd75a7b106872dcc2
Marma92 <elbaz.amram@gmail.com>
2016-11-06 13:13:12 +0100

    adding some code files to define the project's structure

---
+++ functions.py
@@ -0,0 +1,1 @@
+#This file will contains all the functions used by the generator. May be coupled to other functions files in the future. Enjoy.
---
+++ generator.py
@@ -0,0 +1,1 @@
+#This file will contain the main, will ask you to do some choices to generate a project that fits to your needs
